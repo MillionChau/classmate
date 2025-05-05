cc9cbf5cb63438cda65a9beb1e690f36dbcccd3b
Million Chau <thuyhanh4704@gmail.com>
2025-05-05 20:19:16 +0700

    Resolve merge conflicts

--- classmate_app/lib/services/auth_service.dart
+++ classmate_app/lib/services/auth_service.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
+// ignore: depend_on_referenced_packages
 import 'package:http/http.dart' as http;
 
 class AuthService {
   static const String baseUrl = 'http://localhost:8080/api/auth';
 
-  static Future<Map<String, dynamic>> login(String username, String password) async {
+  static Future<Map<String, dynamic>> login(
+      String username, String password) async {
     final response = await http.post(
       Uri.parse('$baseUrl/login'),
       headers: {'Content-Type': 'application/json'},
